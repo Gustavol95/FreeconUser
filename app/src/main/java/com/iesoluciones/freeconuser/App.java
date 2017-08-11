4a58c9192bcbb17a89a588d9b709c411e774d124
IE Developer <tavols889@gmail.com>
2017-08-11 09:55:13 -0600

    Commit

--- app/src/main/java/com/iesoluciones/freeconuser/App.java
+++ app/src/main/java/com/iesoluciones/freeconuser/App.java
@@ -1,8 +1,15 @@
 package com.iesoluciones.freeconuser;
 
+import android.app.Application;
+
 /**
  * Created by iedeveloper on 11/08/17.
  */
 
-public class App {
+public class App extends Application {
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
